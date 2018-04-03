b2cd4a18b8e2c97e92e7fa51e75818a8d22f4381
Ariana Berger <ariana.berger@gmail.com>
2018-04-03 13:52:48 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,10 @@ class CostumeStore < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
-      t.float :price
-      t.string :size
-      t.string :image_url
-      t.timestamps
+      # t.float :price
+      # t.string :size
+      # t.string :image_url
+      # t.timestamps
     end
   end
 end
