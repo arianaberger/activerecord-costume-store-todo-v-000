3aa146a117688625b8c2cf30799436e0604da60b
Ariana Berger <ariana.berger@gmail.com>
2018-04-03 15:09:50 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -32,6 +26,14 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
   end
 
 end
