d9d5a70fc777937f5b2241d2554b420d275c4e06
Ariana Berger <ariana.berger@gmail.com>
2018-04-03 14:10:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      # t.string :image_url
+      t.integer :costume_inventory
       # t.timestamps
     end
   end
